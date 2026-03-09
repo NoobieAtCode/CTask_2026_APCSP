0b54dab22ef6fd6b81459783e08019e1154f0cff
NoobieAtCode <136034949+NoobieAtCode@users.noreply.github.com>
2026-03-09 20:53:16 +0000

    3/9/2026-1

---
+++ parser.py
@@ -0,0 +1,16 @@
+### WORK ON THIS (the parser for the base-n to base-n operation)
+
+base_n1 = 2
+base_n2 = 10
+test_input_base_n = 10010101
+
+def convert_bases(basen1: int, basen2: int, inputbase_n):
+    # Convert both inputs to List[string]
+    # Invert both lists 
+    inputbase_n1_list = list(str(inputbase_n))
+    return inputbase_n1_list
+
+def invertlist(listinput: list):
+    return
+
+print(convert_bases(base_n1, base_n2, test_input_base_n))
